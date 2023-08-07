41760d7bad5cc08fe4db01ed7597ba44afdd90f4
Jhustyn Carvajal <christopherarias2002@gmail.com>
2023-08-07 13:41:52 -0500

    Buttons Screen finish

--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -1,3 +1,5 @@
+//Archivo de barril para las pantallas
+
 export 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 export 'package:widgets_app/presentation/screens/cards/cards_screen.dart';
 export 'package:widgets_app/presentation/screens/home/home_screen.dart';
